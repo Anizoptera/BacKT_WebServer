34c7ca0bcb365d55bff76af2a7023fbdfe22ad39
Oleg Cherr <olegcherr@gmail.com>
2016-12-13 13:38:54 +0400

    [BKT_WebServer] string-utils have become "internal"

--- src/main/kotlin/azadev/backt/webserver/utils/string.kt
+++ src/main/kotlin/azadev/backt/webserver/utils/string.kt
@@ -1,16 +1,22 @@
 package azadev.backt.webserver.utils
 
 
-fun <T : CharSequence> T?.notNullOrEmpty(): T? = if (!isNullOrEmpty()) this else null
-fun <T : CharSequence> T?.notNullOrBlank(): T? = if (!isNullOrBlank()) this else null
+internal fun <T : CharSequence> T?.notNullOrEmpty(): T? = if (!isNullOrEmpty()) this else null
+internal fun <T : CharSequence> T?.notNullOrBlank(): T? = if (!isNullOrBlank()) this else null
 
-fun <T : CharSequence> T?.notEmptyElse(default: T): T = if (this != null && this.isNotEmpty()) this else default
-
-
-fun Any?.toTrimmedString(): String = this?.toString()?.trim() ?: ""
-fun Any?.toNotEmptyString(): String? = this?.toString()?.notNullOrEmpty()
-fun Any?.toNotEmptyTrimmedString(): String? = this?.toString()?.trim()?.notNullOrEmpty()
+internal fun <T : CharSequence> T?.notEmptyElse(default: T): T = if (this != null && this.isNotEmpty()) this else default
 
 
-fun Char.isLetterOrDigitASCII()
+internal fun Any?.toTrimmedString(): String = this?.toString()?.trim() ?: ""
+internal fun Any?.toNotEmptyString(): String? = this?.toString()?.notNullOrEmpty()
+internal fun Any?.toNotEmptyTrimmedString(): String? = this?.toString()?.trim()?.notNullOrEmpty()
+
+
+internal fun <T : CharSequence> T?.ifNotNullOrEmpty(fn: T.()->Unit): T? {
+	if (this != null && this.isNotEmpty()) fn(this)
+	return this
+}
+
+
+internal fun Char.isLetterOrDigitASCII()
 		= this >= '0' && this <= '9' || this >= 'A' && this <= 'Z' || this >= 'a' && this <= 'z'

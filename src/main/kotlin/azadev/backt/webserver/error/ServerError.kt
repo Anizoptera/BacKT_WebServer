1e229010417db1ec5dccd3d5b176a81664e8c186
Oleg Cherr <olegcherr@gmail.com>
2017-06-26 17:17:33 +0400

    [BKT_WebServer] 'ServerError' improved – ability to add custom details into an error-instance

--- src/main/kotlin/azadev/backt/webserver/error/ServerError.kt
+++ src/main/kotlin/azadev/backt/webserver/error/ServerError.kt
@@ -3,17 +3,49 @@ package azadev.backt.webserver.error
 
 interface ServerError
 {
-	val type: CharSequence
-	val code: CharSequence
+	val type: String
+	val code: String
 
-	val summary: String get() = "${javaClass.simpleName}($type;$code)"
+	val details: Any? get() = null
+
+	val summary: String get() = makeSummary(detailed = false)
+	val detailedSummary: String get() = makeSummary(detailed = true)
+
+
+	operator fun invoke(details: Any?) = create(type, code, details)
+
+	infix fun eq(other: ServerError) = type == other.type && code == other.code
+
+
+	private fun makeSummary(detailed: Boolean): String {
+		val sb = StringBuilder()
+
+		sb.append(javaClass.simpleName)
+		sb.append('(').append(type).append(';').append(code)
+
+		if (details != null)
+			sb.append(";{has details}")
+
+		sb.append(')')
+
+		if (detailed && details != null)
+			sb.append('\n').append(details)
+
+		return sb.toString()
+	}
 
 
 	companion object
 	{
-		fun create(type: CharSequence, code: CharSequence) = object : ServerError {
-			override val type = type
-			override val code = code
+		fun create(type: String, code: String) = object : ServerError {
+			override val type get() = type
+			override val code get() = code
+		}
+
+		fun create(type: String, code: String, details: Any?) = object : ServerError {
+			override val type get() = type
+			override val code get() = code
+			override val details get() = details
 		}
 	}
 }
